1ff06ed4be67f81f19804391fa4b2d9cdcda831b
Stefania Izzo <stefania.izzo@sorint.it>
2018-04-20 21:10:40 +0200

    Improvement

--- src/main/java/it/izzonline/simplecrypt/Simplecrypt.java
+++ src/main/java/it/izzonline/simplecrypt/Simplecrypt.java
@@ -1,8 +1,11 @@
 package it.izzonline.simplecrypt;
 
-import java.security.Key;
+import java.security.NoSuchAlgorithmException;
 
 import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 import org.apache.commons.codec.binary.Base64;
@@ -10,21 +13,31 @@ import org.apache.commons.codec.binary.Base64;
 public class Simplecrypt {
 
 	private static final String AES = "AES";
+	private static final String ALGORITHM = "AES/CBC/PKCS5Padding";
+	private static final byte[] IV = { 3, 0, 0, 6, 1, 9, 8, 0, 1, 2, 0, 9, 7, 6, 6, 6 };
 
-	public String encrypt(final String clearText, final String key) throws Exception {
-		Key aesKey = new SecretKeySpec(key.getBytes(), AES);
-		Cipher cipher = Cipher.getInstance(AES);
-		cipher.init(Cipher.ENCRYPT_MODE, aesKey);
+	public static String encrypt(final String clearText, final SecretKey key) throws Exception {
+		IvParameterSpec ivspec = new IvParameterSpec(IV);
+		Cipher cipher = Cipher.getInstance(ALGORITHM);
+		cipher.init(Cipher.ENCRYPT_MODE, key, ivspec);
 		byte[] encrypted = cipher.doFinal(clearText.getBytes());
 		return new String(Base64.encodeBase64(encrypted));
 	}
 
-	public String decrypt(final String encryptedText, final String key) throws Exception {
-		Cipher cipher = Cipher.getInstance(AES);
-		Key aesKey = new SecretKeySpec(key.getBytes(), AES);
-		cipher.init(Cipher.DECRYPT_MODE, aesKey);
+	public static String decrypt(final String encryptedText, final SecretKey key) throws Exception {
+		SecretKeySpec spec = new SecretKeySpec(key.getEncoded(), AES);
+		Cipher cipher = Cipher.getInstance(ALGORITHM);
+		IvParameterSpec ivspec = new IvParameterSpec(IV);
+		cipher.init(Cipher.DECRYPT_MODE, spec, ivspec);
 		String decrypted = new String(cipher.doFinal(Base64.decodeBase64(encryptedText.getBytes())));
 		return decrypted;
+	}
+
+	public static SecretKey generateKey() throws NoSuchAlgorithmException {
+		KeyGenerator keyGenerator = KeyGenerator.getInstance(AES);
+		keyGenerator.init(128);
+		SecretKey key = keyGenerator.generateKey();
+		return key;
 	}
 
 }
